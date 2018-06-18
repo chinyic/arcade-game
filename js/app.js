14cbce2908882e455636cdaacb2a40292b5d5679
Chinyi <chinyi.cc@gmail.com>
2018-06-18 11:23:56 -0700

    create new player variable with x and y coordinates

--- js/app.js
+++ js/app.js
@@ -38,11 +38,13 @@ var Player = function(x, y) {
   this.player = 'images/char-horn-girl.png';
 }
 
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [];
 // Place the player object in a variable called player
-
+var player = new Player (202, 405);
 
 
 // This listens for key presses and sends the keys to your
